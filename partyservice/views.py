734d3357ad3aaaca02878b1c5116cfb41ad11c4d
cptran777 <cptran14@gmail.com>
2016-11-02 20:42:50 -0700

    create basic view and url route

--- partyservice/views.py
+++ partyservice/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+
+import json
 
 # Create your views here.
+def index(request):
+	if (request.method == 'GET'):
+		return JsonResponse({ 'message': 'This is an api for Smite Party' })
+	else:
+		return JsonResponse({ 'message': 'Please make a GET request for gods' })
