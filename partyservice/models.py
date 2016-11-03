befa7e65d498860606959d2503369728cc96602d
cptran777 <cptran14@gmail.com>
2016-11-02 23:15:29 -0700

    create models for the gods and roles and set up relationship

--- partyservice/models.py
+++ partyservice/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Gods(models.Model):
+	god_name = models.CharField(max_length = 30)
+	god_type = models.CharField(max_length = 15)
+	god_class = models.CharField(max_length = 30)
+	god_image = models.CharField(max_length = 150)
+
+class Roles(models.Model): 
+	role_name = models.CharField(max_length = 15)
+	role_gods = models.ManyToManyField(Gods)
